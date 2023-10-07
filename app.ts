4d04aa4825fd72d979f41d689515e96645c1a904
champkeh <champion.ge@gmail.com>
2023-10-07 15:04:35 +0800

    update

--- app.ts
+++ app.ts
@@ -4,18 +4,18 @@ import {read, write, clear} from "./kv.ts"
 Deno.serve(async (req: Request) => {
     const url = new URL(req.url)
     const token = url.searchParams.get('token')
-    console.log('url: ', req.url)
-    console.log('token: ', token)
 
-    if (url.pathname === '/read') {
-        console.log('read')
-        // await read()
-    } else if (url.pathname === '/write') {
-        console.log('write')
-        // await write()
-    } else if (url.pathname === '/clear') {
-        console.log('clear')
-        // await clear()
+    if (token === '666') {
+        if (url.pathname === '/read') {
+            console.log('read')
+            await read()
+        } else if (url.pathname === '/write') {
+            console.log('write')
+            await write()
+        } else if (url.pathname === '/clear') {
+            console.log('clear')
+            await clear()
+        }
     }
 
     return new Response(`response to ${req.url}`)
--- kv.ts
+++ kv.ts
@@ -1,4 +1,22 @@
-const kv = await Deno.openKv();
+import * as dotenv from "https://deno.land/std@0.202.0/dotenv/mod.ts";
+
+
+/**
+ * 是否在deploy中运行代码
+ */
+export function runInDenoDeploy() {
+    const deploymentId = Deno.env.get("DENO_DEPLOYMENT_ID")
+    return !!deploymentId
+}
+
+let kv: Deno.Kv
+if (runInDenoDeploy()) {
+    kv = await Deno.openKv();
+} else {
+    const env = await dotenv.load()
+    Deno.env.set('DENO_KV_ACCESS_TOKEN', env["DENO_KV_ACCESS_TOKEN"])
+    kv = await Deno.openKv("https://api.deno.com/databases/d64e0c74-717b-49b3-a4ca-d3212045cb8a/connect");
+}
 
 // units/day (4kb)
 export async function read() {
