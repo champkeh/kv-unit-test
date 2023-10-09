21900b1dc15d8875b612552137311b207ef13717
champkeh <champion.ge@gmail.com>
2023-10-09 10:15:15 +0800

    update

--- ops/enqueue.ts
+++ ops/enqueue.ts
@@ -2,37 +2,39 @@ import kv from "../kv.ts"
 import {_10B, _1K, _2K, _64K} from "./const.ts";
 
 
+// 18 write units
 export async function enqueue1() {
     await kv.enqueue(_10B, {
         delay: 10 * 1000,
-        keysIfUndelivered: [["delivered"]]
+        keysIfUndelivered: [["delivered for enqueue1"]]
     })
 }
 
+// 22
 export async function enqueue2() {
     await kv.enqueue(_1K, {
         delay: 10 * 1000,
-        keysIfUndelivered: [["delivered"]]
+        keysIfUndelivered: [["delivered for enqueue2"]]
     })
 }
 
 export async function enqueue3() {
     await kv.enqueue(_2K, {
         delay: 10 * 1000,
-        keysIfUndelivered: [["delivered"]]
+        keysIfUndelivered: [["delivered for enqueue3"]]
     })
 }
 
 export async function enqueue4() {
     await kv.enqueue(_64K, {
         delay: 10 * 1000,
-        keysIfUndelivered: [["delivered"]]
+        keysIfUndelivered: [["delivered for enqueue4"]]
     })
 }
 
 export async function enqueue5() {
     await kv.enqueue(_64K, {
         delay: 10 * 1000,
-        keysIfUndelivered: [["delivered"]]
+        keysIfUndelivered: [["delivered for enqueue5"]]
     })
 }
